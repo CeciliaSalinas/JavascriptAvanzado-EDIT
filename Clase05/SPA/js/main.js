6c208ecf4f9eaa2aae486db2631f29628752ef62
CeciliaSalinas <salinascecilia.mdl@gmail.com>
2025-05-09 18:52:27 -0300

    historial con hash

--- Clase05/SPA/js/main.js
+++ Clase05/SPA/js/main.js
@@ -25,35 +25,138 @@ function cargarNavBar(cb){
 }
 
 
-cargarNavBar(getPlantillas)
+/* cargarNavBar(getPlantillas) */
+/* cargarNavBar(getPlantillasSinHistory) */
+cargarNavBar(getPlantillasConHistoryHash)
 
 
-function getPlantillas(){
+/* ---------------------------------------------------------------------- */
+/*        Manejo de la vista dinámica (SPA) CON HISTORIAL HASH (#)         */ 
+/* ---------------------------------------------------------------------- */
+
+function getPlantillasConHistoryHash(){
     const links = document.querySelectorAll('a')
     const main = document.querySelector('main')
-    console.log(links)
+    /* console.log(links) */
 
-    links.forEach( link => {
-        link.addEventListener('click', e => {
-            e.preventDefault()
+        /* ---------------------------------------------------------------------- */
+        /*                     Cargamos la vista inicial                          */ 
+        /* ---------------------------------------------------------------------- */
 
-            let id = link.id
-            console.log(id)
+       /*  let archivo = getNombreArchivo('home') */
+        /* console.log(archivo) */
+/* 
+        let xhr = ajax(archivo)
+        xhr.addEventListener('load', () => {
+            if(xhr.status == 200){
+            main.innerHTML = xhr.response
+            
+        }
+        }) */
 
-            let archivo = getNombreArchivo(id)
-            console.log(archivo)
+
+        /* ---------------------------------------------------------------------- */
+        /*     Cargamos la vista seleccionada desde la barra de navegacion        */ 
+        /* ---------------------------------------------------------------------- */
+
+            links.forEach( link => {
+                link.addEventListener('click', e => {
+                    e.preventDefault()
+
+                    let id = link.id
+                    /* console.log(id) */
+                    location.hash = id
+
+                    let archivo = getNombreArchivo(id)
+                    /* console.log(archivo) */
+
+                    let xhr = ajax(archivo)
+                    xhr.addEventListener('load', () => {
+                        if(xhr.status == 200){
+                        main.innerHTML = xhr.response
+                        
+                }
+            })
+
+                })
+            })
+
+            window.addEventListener('hashchange', () => {
+                console.log('Cambio el hash url')
+
+                let id = location.hash.slice(1)
+                
+            let archivo = getNombreArchivo('home')
+            /* console.log(archivo) */
 
             let xhr = ajax(archivo)
             xhr.addEventListener('load', () => {
                 if(xhr.status == 200){
                 main.innerHTML = xhr.response
                 
-        }
-    })
+            }
+            })
+                })
 
+            }
+
+/* ---------------------------------------------------------------------- */
+/*        Manejo de la vista dinámica (SPA) SIN HISTORIAL                 */ 
+/* ---------------------------------------------------------------------- */
+
+function getPlantillasSinHistory(){
+    const links = document.querySelectorAll('a')
+    const main = document.querySelector('main')
+    /* console.log(links) */
+
+        /* ---------------------------------------------------------------------- */
+        /*                     Cargamos la vista inicial                          */ 
+        /* ---------------------------------------------------------------------- */
+
+        let archivo = getNombreArchivo('home')
+        /* console.log(archivo) */
+
+        let xhr = ajax(archivo)
+        xhr.addEventListener('load', () => {
+            if(xhr.status == 200){
+            main.innerHTML = xhr.response
+            
+        }
         })
-    })
-}
+
+
+        /* ---------------------------------------------------------------------- */
+        /*     Cargamos la vista seleccionada desde la barra de navegacion        */ 
+        /* ---------------------------------------------------------------------- */
+
+            links.forEach( link => {
+                link.addEventListener('click', e => {
+                    e.preventDefault()
+
+                    let id = link.id
+                    /* console.log(id) */
+
+                    let archivo = getNombreArchivo(id)
+                    /* console.log(archivo) */
+
+                    let xhr = ajax(archivo)
+                    xhr.addEventListener('load', () => {
+                        if(xhr.status == 200){
+                        main.innerHTML = xhr.response
+                        
+                }
+            })
+
+                })
+            })
+
+        }
+
+
+
+
+
+
 
 
 
